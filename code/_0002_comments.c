d07ec758ef56d2d7373da334ff1ecd54ef5a1f4b
raghavtwenty <raghavtwenty@gmail.com>
2024-07-10 19:31:53 +0530

    added comments

---
+++ code/_0002_comments.c
@@ -0,0 +1,25 @@
+/*
+Filename: _0002_comments.c
+Title: Comments in c
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 10, 2024 | Last Updated: July 10, 2024
+# Language: C | Version: clang-14
+*/
+
+/*
+Multi-line comment with developer details on the top of the program
+is called docstring
+*/
+
+// Required imports - Single line comment starts with //
+#include <stdio.h>
+
+// Main function
+int main() {
+    /*
+    Multi-line comment starts and ends with /**/
+    */
+    printf("Hello, World!\n");
+    // Return 0 stops the program
+    return 0; 
+}
