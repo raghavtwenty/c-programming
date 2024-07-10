1724717354c2bbcf3c2aa2540f6a5a9d91fdb842
raghavtwenty <raghavtwenty@gmail.com>
2024-07-10 19:31:38 +0530

    added hello world

---
+++ code/_0001_hello_world.c
@@ -0,0 +1,18 @@
+/*
+Filename: _0001_hello_world.c
+Title: First program in c
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 10, 2024 | Last Updated: July 10, 2024
+# Language: C | Version: clang-14
+*/
+
+// Required imports
+#include <stdio.h>
+
+// Main function
+int main() {
+    // Print to the console
+    printf("Hello, World!\n");
+    // Return 0 stops the program
+    return 0; 
+}
