509e91584e3bcceed80000471c227716d08204c9
raghavtwenty <raghavtwenty@gmail.com>
2024-07-25 20:28:21 +0530

    added char to num

---
+++ code/_0005_char_num.c
@@ -0,0 +1,19 @@
+/*
+Filename: _0005_char_num.c
+Title: Character to number and vice-versa
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 25, 2024 | Last Updated: July 25, 2024
+Language: C | Version: clang-14
+*/
+
+// Required imports
+#include <stdio.h>
+
+// Main function
+int main() {
+    char n = 65;           // Initialize n with the ASCII value 65 which corresponds to 'A'
+
+    // Convert the integer to a character and print it
+    printf("Integer as character: %c\n", n); 
+    return 0;
+}
