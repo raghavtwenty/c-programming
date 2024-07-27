6a7435c16bf7f040e3977fb242e5499f70913365
raghavtwenty <raghavtwenty@gmail.com>
2024-07-27 08:08:02 +0530

    added if condition

---
+++ code/_0012_if_condition.c
@@ -0,0 +1,21 @@
+/*
+Filename: _0012_if_condition.c
+Title: If condition
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 27, 2024 | Last Updated: July 27, 2024
+Language: C | Version: clang-14
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+
+    // If statement
+    if (num > 0) {
+        // If true, executes
+        printf("The number is positive.\n");
+    }
+
+    return 0;
+}
