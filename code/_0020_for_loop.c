91734bb3d780bb1a1b8a4cccae033584132f0ae7
raghavtwenty <raghavtwenty@gmail.com>
2024-07-31 00:16:38 +0530

    added for loop

---
+++ code/_0020_for_loop.c
@@ -0,0 +1,20 @@
+/*
+Filename: _0020_for_loop.c
+Title: for loop
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 31, 2024 | Last Updated: July 31, 2024
+Language: C | Version: clang-14
+*/
+
+// Required imports
+#include <stdio.h>
+
+// Main
+int main() {
+    // for loop - initial value, stop value, step value
+    for (int count = 1; count <= 5; count++) {
+        printf("Count: %d\n", count);
+    }
+
+    return 0;
+}
