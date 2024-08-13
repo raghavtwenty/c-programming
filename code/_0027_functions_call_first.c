a850cbd46a2c226315ebb29dd707636d82d47156
raghavtwenty <raghavtwenty@gmail.com>
2024-08-13 10:09:44 +0530

    added functions call first

---
+++ code/_0027_functions_call_first.c
@@ -0,0 +1,26 @@
+/*
+Filename: _0027_functions_call_first.c
+Title: Function calls
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: August 13, 2024 | Last Updated: August 13, 2024
+Language: C | Version: clang-14
+*/
+
+// Required imports
+#include <stdio.h>
+
+// Function declaration
+void greet();
+
+// Main
+int main() {
+    // Function call
+    greet();
+
+    return 0;
+}
+
+// Function body
+void greet() {
+    printf("Hello, World!\n");
+}
